ad88d4ff7d9c71802b3be1a8d5fc68e1d09cfc58
Tishanth-07 <139070316+Tishanth-07@users.noreply.github.com>
2025-10-20 23:23:59 +0530

    Backend connect with database

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,15 +1,17 @@
-using YourApp.Models;
-using YourApp.Services;
+using backend.Models;
+using backend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Bind MongoDb settings
 builder.Services.Configure<MongoDbSettings>(
     builder.Configuration.GetSection("MongoDbSettings"));
+#pragma warning disable CS8604 // Possible null reference argument.
 builder.Services.AddSingleton<MongoDbSettings>(
     builder.Configuration.GetSection("MongoDbSettings").Get<MongoDbSettings>());
+#pragma warning restore CS8604 // Possible null reference argument.
 
-// Register service
+// Register your services
 builder.Services.AddSingleton<TestService>();
 
 builder.Services.AddControllers();
@@ -17,7 +19,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 app.UseSwagger();
 app.UseSwaggerUI();
 app.MapControllers();
+
 app.Run();
+
+
