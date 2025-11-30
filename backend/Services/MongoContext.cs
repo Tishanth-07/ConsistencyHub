40917770095afc2cd575abf5285e428ab8128788
Tishanth-07 <139070316+Tishanth-07@users.noreply.github.com>
2025-11-30 23:23:08 +0530

    update the Mongodb contex

--- backend/Services/MongoContext.cs
+++ backend/Services/MongoContext.cs
@@ -16,14 +16,10 @@ namespace ConsistencyHub.Services
             var databaseName = mongoSettings.GetValue<string>("DatabaseName");
 
             if (string.IsNullOrEmpty(connectionString))
-#pragma warning disable CA2208 // Instantiate argument exceptions correctly
                 throw new ArgumentNullException(nameof(connectionString), "MongoDB connection string is missing in appsettings.json");
-#pragma warning restore CA2208 // Instantiate argument exceptions correctly
 
             if (string.IsNullOrEmpty(databaseName))
-#pragma warning disable CA2208 // Instantiate argument exceptions correctly
                 throw new ArgumentNullException(nameof(databaseName), "MongoDB database name is missing in appsettings.json");
-#pragma warning restore CA2208 // Instantiate argument exceptions correctly
 
             var client = new MongoClient(connectionString);
             _database = client.GetDatabase(databaseName);
